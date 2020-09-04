dc50bbba627478feb20e5406867c8c885426a24f
Zontreck <tara@zontreck.dev>
2020-09-04 02:46:57 -0700

    Start adding functions

---
+++ AutoUpdaterFlag.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibZNI
+{
+    [AttributeUsage(AttributeTargets.Assembly)]
+    public class AutoUpdater : Attribute
+    {
+        public string AutoUpdaterProjectPath;
+        public bool AutoUpdateEnabled = false;
+        public string AutoUpdateArtifact = "";
+        public AutoUpdater(string path, string artifact)
+        {
+            AutoUpdaterProjectPath = path;
+            AutoUpdateEnabled = true;
+            AutoUpdateArtifact = artifact;
+        }
+    }
+}
